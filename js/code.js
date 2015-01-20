00838e3fcbd0f3b3b78920770807f4812edc89dd
Nick Meldrum <nick@nickmeldrum.com>
2015-01-20 22:41:32 +0000

    added xhr functions

--- js/code.js
+++ js/code.js
@@ -1,19 +1,50 @@
 window.Mouse = window.Mouse || {};
 
 window.Mouse.Display = function () {
-    var dataset = [ { "Label": "Branch" }, { "Label": "Version" }, { "Label": "Version" } ];
+    return {
+        execute: function() {
+            var dataset = [ { "Label": "Branch" }, { "Label": "Version" }, { "Label": "Version" } ];
 
-    d3.select("svg").selectAll("circle")
-        .data(dataset)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d, i) { return (i * 50) + 50; })
-        .attr("cy", 50)
-        .attr("r", 20)
-        .attr("fill", function(d) {
-            if (d.Label == "Branch") return "red";
-            else if (d.Label == "Version") return "purple";
-            else return "gray";
-        });
+            d3.select("svg").selectAll("circle")
+                .data(dataset)
+                .enter()
+                .append("circle")
+                .attr("cx", function(d, i) { return (i * 50) + 50; })
+                .attr("cy", 50)
+                .attr("r", 20)
+                .attr("fill", function(d) {
+                    if (d.Label == "Branch") return "red";
+                    else if (d.Label == "Version") return "purple";
+                    else return "gray";
+                });
+        }
+    };
+}();
+
+window.Mouse.Ajax = function() {
+    return {
+        post: function (url, body, success, failure) {
+            var r = new XMLHttpRequest(); 
+            r.open("POST", url, true);
+            r.onreadystatechange = function () {
+                if (r.readyState != 4 || r.status != 200) failure(r); 
+                success(r);
+            };
+            r.send(body);
+       },
+       get: function (url, success, failure) {
+            var r = new XMLHttpRequest(); 
+            r.open("GET", url, true);
+            r.onreadystatechange = function () {
+                if (r.readyState != 4 || r.status != 200) failure(r); 
+                success(r);
+            };
+            r.send();
+       }
+   };
+}();
+
+window.Mouse.Program = function() {
+    window.Mouse.Display.execute();
 }();
 

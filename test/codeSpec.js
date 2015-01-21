89e596d8eb01e458814191d8379bb164ee0204a7
Nick Meldrum <nick@nickmeldrum.com>
2015-01-21 20:07:24 +0000

    added some test coverage around form url encoded converter

--- test/codeSpec.js
+++ test/codeSpec.js
@@ -4,6 +4,16 @@ describe('Ajax Body Converter', function () {
         expect(this.jsonResult).toEqual('name=Nick');
     });
 
+    it('converts from json with spaces in value to form-url-encoded', function () {
+        this.jsonResult = window.Mouse.Ajax.BodyConverter.fromJsonToFormUrlEncoded([{Name: 'name', Value: 'Nick Meldrum'}]);
+        expect(this.jsonResult).toEqual('name=Nick+Meldrum');
+    });
+
+    it('converts 2 json values to form-url-encoded', function () {
+        this.jsonResult = window.Mouse.Ajax.BodyConverter.fromJsonToFormUrlEncoded([{Name: 'name', Value: 'Nick'}, {Name: 'age', Value: 21}]);
+        expect(this.jsonResult).toEqual('name=Nick&age=21');
+    });
+
     it('allows us to pass in a json object instead of an array when there is 1 value', function () {
         this.jsonResult = window.Mouse.Ajax.BodyConverter.fromJsonToFormUrlEncoded({Name: 'name', Value: 'Nick'});
         expect(this.jsonResult).toEqual('name=Nick');
